4fd02810c3638298816117db94a2215d2849b682
Ilya Sokolov <developer.sokolov@gmail.com>
2018-04-18 16:38:35 +0300

    changed fmt to log

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"gopkg.in/telegram-bot-api.v4"
 	"log"
-	"fmt"
 	"io/ioutil"
 	"encoding/xml"
 )
@@ -23,32 +22,28 @@ func init() {
 }
 
 func main() {
-
 	bot, err := tgbotapi.NewBotAPI(config.TelegramBotToken)
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
 	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
-		botSay(bot, update)
 
+		botSay(bot, update)
 	}
 }
 
 func botSay(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-
 	if len(update.Message.Text) != 0 {
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 		msg := tgbotapi.MessageConfig{}
@@ -58,14 +53,11 @@ func botSay(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, " Java, Kotlin, Go! We are doing good!")
 			msg.ReplyToMessageID = update.Message.MessageID
 		}
-
 		bot.Send(msg)
-
 	} else if update.Message.Sticker != nil {
 		msg := tgbotapi.NewStickerShare(update.Message.Chat.ID, update.Message.Sticker.FileID)
-		fmt.Printf("id = %s \n", update.Message.Sticker.FileID)
+		log.Printf("Sticker id = %s \n", update.Message.Sticker.FileID)
 		msg.ReplyToMessageID = update.Message.MessageID
 		bot.Send(msg)
 	}
-
 }
